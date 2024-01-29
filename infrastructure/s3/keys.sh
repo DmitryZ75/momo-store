a15c4842fdb53f7798c0a4379dc500ba873b33dc
Dmitry Zhagrin <mail@dmitryz.com>
2024-01-29 14:46:24 +0300

    Delete keys.sh

--- infrastructure/s3/keys.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# экспорт ключей для кранения terraform state
-
-export AWS_SECRET_ACCESS_KEY=$(terraform output -raw s3-secret-key)
-export AWS_ACCESS_KEY_ID=$(terraform output -raw s3-access-key)
